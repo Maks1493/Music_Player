fe6a72d256368cece3ae4efac9a13a9ed8077779
Mask1493 <maksmazurekwp@gmail.com>
2023-07-28 11:52:43 +0200

    changed visuals

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ function redirectToPage3() {
   window.location.href = "music3.html";
 }
 
+var textWrapper = document.querySelector('.ml13');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+.add({
+    targets: '.ml13 .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i
+}).add({
+    targets: '.ml13 .letter',
+    translateX: [0,-30],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1100,
+    delay: (el, i) => 100 + 30 * i
+});
